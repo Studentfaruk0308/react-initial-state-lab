20abd18439fe38502371d917fe411c35fc124cd3
faruk <studentfaruk0308@gmail.com>
2022-05-09 23:06:43 +1000

    After Completion

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,32 @@
 // your ImageSlider code here!
+// import React from 'react'
+
+// export default function ImageSlider() {
+//     constructor(){
+//         super();
+//         this.state = {
+//             currentSlideIndex = 0;
+//         }
+//     }
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+import React, {Component} from 'react';
+
+export default class ImageSlider extends Component{
+    constructor(props){
+        super();
+
+        this.state ={currentSlideIndex: 0}
+    }
+    render(){
+        return (
+            <div>{this.state.currentSlideIndex}</div>
+        )
+    }
+    
+}
